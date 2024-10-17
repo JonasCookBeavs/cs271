7660fb9ba701e68b2a65e8b96a2d8fe3c981d7d5
Jonas Cook <cookjona@oregonstate.edu>
2024-10-17 08:57:13 -0700

    Created fin and fout

--- cploration/c03/main.c
+++ cploration/c03/main.c
@@ -5,8 +5,27 @@
  * [TERM] FALL 2024
  * 
  ****************************************/
- 
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LINE_LENGTH  200
+int main(int argc, const char *argv[])
 {	
+	if(argc != 2){
+		printf("Usage: %s [filename]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	FILE *fin = fopen( argv[1], "r" );
+	
+	if (fin == NULL){
+		perror("Unable to open file!");
+		exit(EXIT_FAILURE);	
+	}
+	
+	char out_file[MAX_LINE_LENGTH];
+	strcpy(out_file, argv[1]);
+	strcat(out_file, ".echo");
+	FILE *fout = fopen(out_file, "w+");
+	
 	return 1;
 }
